c45c17f2d13a47a9f8cd07cf155fb6cd772048b6
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-16 19:16:56 +0100

    math operators

--- FundamentalsPart1/script.js
+++ FundamentalsPart1/script.js
@@ -50,7 +50,7 @@ console.log(typeof year);
 
 // null is not an object its bug :3
 console.log(typeof null);
-*/
+
 
 let age = 30;
 age = 31;
@@ -64,6 +64,30 @@ job = 'teacher';
 
 lastName = 'Nikolic';
 console.log(lastName);
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// 2 ** 3 means 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
 
 
 
