62d88b4f4ce850233455ad0a7ef1e542b80a7b25
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-16 18:17:01 +0100

    end of first lecture

---
+++ FundamentalsPart1/script.js
@@ -0,0 +1,50 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = 'Shone';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name convention
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
