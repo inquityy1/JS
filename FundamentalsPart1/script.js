678d5153c69884fa535a5720528132c3ad7d8ec2
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-17 01:07:10 +0100

    coercion

--- FundamentalsPart1/script.js
+++ FundamentalsPart1/script.js
@@ -217,6 +217,19 @@ console.log(typeof NaN);
 console.log(String(23), 23);
 
 //type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+// + means that string + next string
+console.log('23' + '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+
+
 
 
 
