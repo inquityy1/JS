3bd430d880c1eb8936056c594be28a7e91a43aa1
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-17 19:39:47 +0100

    converting switch case to if else case

--- FundamentalsPart1/script.js
+++ FundamentalsPart1/script.js
@@ -363,6 +363,20 @@ switch(day) {
 		console.log('Not a valid day!');
 }
 
+if (day === 'monday') {
+	console.log('Plan course structure');
+	console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+	console.log('prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+	console.log('write code examples');
+} else if (day === 'friday') {
+	console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+	console.log('Enjoy the weekend :D');
+} else {
+	console.log('Not a valid day!');
+}
 
 
 
