30cfba342dc6046dcc49fe04832112bce585daf7
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-23 19:38:13 +0100

    first function

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -1,11 +1,47 @@
+/* 
 'use strict';
 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
+
 if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
 // const if = 23;
+*/
+
+function logger() {
+	console.log('My name is jonas');
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
