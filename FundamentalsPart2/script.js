8266dc8b456c3cfb9c98574c9e2e35f8cc3d2639
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-23 20:02:15 +0100

    function with parameters

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -22,6 +22,16 @@ logger();
 logger();
 logger();
 
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
 
 
 
