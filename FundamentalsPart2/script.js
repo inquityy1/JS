ec2b02c13322c947103eeaaef3d11ddc905863ec
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-27 18:24:22 +0100

    arrays are 0 based

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -158,6 +158,11 @@ const friend3 = 'Peter';
 const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
 
 
 
