11513f6fee92213485ae48e67ed6475a6194ba2d
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-02 22:57:45 +0100

    course aprouch

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -364,8 +364,13 @@ const mark = {
 	mass: 78,
 	height: 1.69,
 	
+//	calcBMI: function() {
+//		return (this.mass / this.height ** 2).toFixed(1);
+//	}
+
 	calcBMI: function() {
-		return (this.mass / this.height ** 2).toFixed(1);
+		this.bmi = this.mass / this.height ** 2;
+		return this.bmi;
 	}
 }
 
@@ -374,17 +379,31 @@ const john = {
 	mass: 92,
 	height: 1.95,
 	
+//	calcBMI: function() {
+//		return (this.mass / this.height ** 2).toFixed(1);
+//	}
+
 	calcBMI: function() {
-		return (this.mass / this.height ** 2).toFixed(1);
+		this.bmi = this.mass / this.height ** 2;
+		return this.bmi;
 	}
 }
+
+mark.calcBMI();
+john.calcBMI();
 
 console.log(mark.calcBMI());
 console.log(john.calcBMI());
 
-console.log(`${mark.calcBMI() > john.calcBMI() ? mark.fullName : john.fullName}'s BMI (${mark.calcBMI() > john.calcBMI() ? mark.calcBMI() : john.calcBMI()}) is higher than ${john.calcBMI() > mark.calcBMI() ? mark.fullName : john.fullName}'s (${john.calcBMI() > mark.calcBMI() ? mark.calcBMI() : john.calcBMI()}) BMI `);
+// console.log(`${(mark.calcBMI() > john.calcBMI()) ? mark.fullName : john.fullName}'s BMI (${(mark.calcBMI() > john.calcBMI()) ? mark.calcBMI() : john.calcBMI()}) is higher than ${(john.calcBMI() > mark.calcBMI()) ? mark.fullName : john.fullName}'s (${(john.calcBMI() > mark.calcBMI()) ? mark.calcBMI() : john.calcBMI()}) BMI `);
 
-
+if (mark.bmi > john.bmi) {
+	console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+	console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+} else {
+	console.log('They have same BMI')
+}
 
 
 
