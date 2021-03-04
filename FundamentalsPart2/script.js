b76ddb6bc5808b2f717059fb489a75c686882c64
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-04 04:48:06 +0100

    dice logic

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -490,15 +490,24 @@ console.log(`--------- Starting ${exercise}`);
 }
 */
 
-for (let rep = 1; rep <= 10; rep++) {
-	console.log(`Lifting weight repetition ${rep}`);
-}
+// for (let rep = 1; rep <= 10; rep++) {
+//	console.log(`Lifting weight repetition ${rep}`);
+// }
 
 let rep = 1;
 while (rep <= 10) {
-	console.log(`Lifting weight repetition ${rep}`);
+	console.log(`WHILE: Lifting weight repetition ${rep}`);
 	rep++;
 }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+	console.log(`You rolled a ${dice}`);
+	dice = Math.trunc(Math.random() * 6) + 1;
+}
+
 
 
 
