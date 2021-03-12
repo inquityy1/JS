a6834dba9b625500edfcf75d0683e1ea99992c25
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-12 17:17:21 +0100

    error handling

--- 3 Developer-Skills/starter/script.js
+++ 3 Developer-Skills/starter/script.js
@@ -1,9 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-if (x === 23) console.log(23);
+// PROBLEM:
+We work for a company building a smart home thermometer. Our most recent task is this: "Give an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const calcAge = birthYear => 2037 - birthYear;
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-console.log();
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
+// - What's a sensor error? and what do do?
+
+// 2) Breaking up into sub-problems
