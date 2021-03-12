e2ef6f9d4ea75740a6e46e7ad8d02fa8290f835c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-12 17:46:18 +0100

    using google search to solve problems

--- 3 Developer-Skills/starter/script.js
+++ 3 Developer-Skills/starter/script.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // PROBLEM:
-We work for a company building a smart home thermometer. Our most recent task is this: "Give an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+// We work for a company building a smart home thermometer. Our most recent task is this: "Give an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
@@ -12,3 +12,30 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - What's a sensor error? and what do do?
 
 // 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO? Just marge two arrays
+// - Merge 2 arrays?
