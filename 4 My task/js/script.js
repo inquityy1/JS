3ccc8c758176339458b6b7ba47f213dffa157196
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-15 23:58:05 +0100

    addEventListener

--- 4 My task/js/script.js
+++ 4 My task/js/script.js
@@ -1,6 +1,12 @@
 function myFunction() {
   const table = document.getElementById("myTable");
   const row = table.insertRow();
-  const cell1 = row.insertCell();
-  cell1.innerHTML = myText.value;
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  cell1.innerHTML = 1;
+  cell2.innerHTML = myText.value;
+  cell3.innerHTML = "dsa";
 }
+
+document.getElementById("submit").addEventListener("click", myFunction);
