2ffbc75cc124d8835d15d4e420be95ce2b6bf9e9
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-15 23:35:47 +0100

    submit text generator in table body

---
+++ 4 My task/js/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  const table = document.getElementById("myTable");
+  const row = table.insertRow();
+  const cell1 = row.insertCell();
+  cell1.innerHTML = myText.value;
+}
