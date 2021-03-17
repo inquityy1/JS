4aaaadccc77a4465a78cf9d502a0568a1c8591d6
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-17 01:00:10 +0100

    adding 8 char for input

--- 4 My task/js/script.js
+++ 4 My task/js/script.js
@@ -4,7 +4,7 @@ function upload() {
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
   const cell3 = row.insertCell(2);
-  cell1.innerHTML = table.rows.length;
+  cell1.innerHTML = table.rows.length - 1;
   cell2.innerHTML = myText.value;
   cell3.innerHTML =
     '<button id="delete" onclick="deleteRow(this)" name="delete">DELETE</button>';
@@ -16,15 +16,3 @@ function deleteRow(r) {
 }
 
 document.getElementById("submit").addEventListener("click", upload);
-
-
-function addRow(dataTable) {
-    var table = document.getElementById('dataTable');
-                var rowCount = table.rows.length;
-               var row = table.insertRow(2);
-                   var colCount = table.rows[3].cells.length;
-                   for(var i=0; i<colCount; i++) {
-                       var newcell = row.insertCell(i);
-                       newcell.innerHTML = table.rows[3].cells[i].innerHTML;
-   
-               }
