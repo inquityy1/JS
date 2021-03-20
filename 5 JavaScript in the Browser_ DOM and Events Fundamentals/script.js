0c5b2a0c3d0b18d11ef0656d7d12629886cd700a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-20 02:16:45 +0100

    messages

--- 5 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
+++ 5 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
@@ -13,17 +13,22 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
-
+    // document.querySelector('.message').textContent = '⛔ No Number!';
+    displayMessage('⛔ No Number!');
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🥳 Correct Number!';
+    // document.querySelector('.message').textContent = '🥳 Correct Number!';
+    displayMessage('🥳 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
