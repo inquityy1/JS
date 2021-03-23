adb05ce10e036518522f6692f448f6760322aa70
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-23 02:47:05 +0100

    classList contains and making modal to close when u click esc

--- 5 JavaScript in the Browser_ DOM and Events Fundamentals/modal/script.js
+++ 5 JavaScript in the Browser_ DOM and Events Fundamentals/modal/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -21,3 +20,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
