613bc4c77e7d839d9902fb891461ef2e434a3aa9
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-25 00:57:27 +0100

    block and function scoped

--- 6 Behind-the-Scenes/script.js
+++ 6 Behind-the-Scenes/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 2001 && birthYear <= 2006) {
+      var millenial = true;
+      const str = `Oh, and you're a elite, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenial);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Nebojsa';
+calcAge(2001);
+// console.log(age);
+// printAge();
