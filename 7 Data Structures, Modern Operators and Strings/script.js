d50a6d74e5f0eb3e6dba3380b3fdad884b3b98f5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-30 01:23:57 +0200

    Spread Operator *...)

--- 7 Data Structures, Modern Operators and Strings/script.js
+++ 7 Data Structures, Modern Operators and Strings/script.js
@@ -43,6 +43,22 @@ const restaurant = {
   },
 };
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+/*
+////////////////////////////////////////////
+// Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
@@ -83,7 +99,7 @@ const {
 } = openingHours;
 console.log(o, c);
 
-/*
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
