28fe60f8df29bc06ef0d51c08c1e78bd3f94a27c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-30 02:36:24 +0200

    copy with spread operator

--- 7 Data Structures, Modern Operators and Strings/script.js
+++ 7 Data Structures, Modern Operators and Strings/script.js
@@ -56,6 +56,18 @@ console.log(1, 2, 7, 8, 9);
 const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 console.log(newMenu);
 
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+
 /*
 ////////////////////////////////////////////
 // Destructuring Objects
