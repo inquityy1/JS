537a3ee4f5402bc920ae8ffd7f32e0d606768289
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-30 03:39:28 +0200

    rest pattern and parameters

--- 7 Data Structures, Modern Operators and Strings/script.js
+++ 7 Data Structures, Modern Operators and Strings/script.js
@@ -49,6 +49,21 @@ const restaurant = {
   },
 };
 
+// SPREAD, becouse on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+/*
+/////////////////////////////////////////////
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
