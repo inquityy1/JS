f465c1ef331c94ac9b5a10ebdea6dcf56c06bef5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-03-31 21:04:07 +0200

    preparing for challenge #2

--- 7 Data Structures, Modern Operators and Strings/script.js
+++ 7 Data Structures, Modern Operators and Strings/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -321,7 +321,7 @@ console.log(i, j, k);
 // default values
 const [p = 1, q = 1, r = 1] = [7];
 console.log(p, q, r);
-
+*/
 
 const game = {
   team1: 'Bayern Munich',
@@ -364,6 +364,8 @@ const game = {
   },
 };
 
+/*
+//////////////////////////////////////////////
 // 1.
 const [players1, players2] = game.players;
 console.log(players1, players2);
