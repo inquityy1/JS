1b98522e174c2e5c09de294f6f5e3e3dcda5f2c6
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-04-01 22:01:00 +0200

    maps and get/set method

--- 7 Data Structures, Modern Operators and Strings/script.js
+++ 7 Data Structures, Modern Operators and Strings/script.js
@@ -53,6 +53,34 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest);
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 22;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.clear();
+console.log(rest);
+console.log(rest.size);
+
+/*
+///////////////////////////////
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -77,6 +105,15 @@ console.log(ordersSet);
 for (const order of ordersSet) console.log(order);
 
 // Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('Jonasschmedtmann').size);
+console.log(new Set('Nebojsa').size);
 
 /*
 ///////////////////////////////////////////////
