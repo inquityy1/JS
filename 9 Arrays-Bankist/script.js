0ba70b84cba0eb9c631a63e5a4ba1bf33dbf1b92
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-04-07 06:11:15 +0200

    filter method

--- 9 Arrays-Bankist/script.js
+++ 9 Arrays-Bankist/script.js
@@ -91,6 +91,7 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -222,3 +223,16 @@ currenciesUnique.forEach(function (value, key, map) {
 //     )}`
 // );
 // console.log(movementsDescriptions);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
