4994a1feaaafffd0a9b77610a07fdea2c1365145
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-04-08 18:30:45 +0200

    challenge #3

--- 9 Arrays-Bankist/script.js
+++ 9 Arrays-Bankist/script.js
@@ -289,8 +289,8 @@ console.log(withdrawals);
 //   else return mov;
 // }, movements[0]);
 // console.log(max);
-
 /*
+
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
@@ -311,7 +311,7 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
-*/
+
 
 const eurToUsd = 1.1;
 console.log(movements);
@@ -326,3 +326,14 @@ const totalDepositsUSD = movements
   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
