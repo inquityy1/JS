6b9edf83532f31ff65016cdc019e14695a9c0920
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-04-23 21:52:36 +0200

    Chainint Promises

--- 14 Asynchronous/script.js
+++ 14 Asynchronous/script.js
@@ -93,7 +93,20 @@ const getCountryDataAndNeighbour = function (country) {
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('portugal');
+// getCountryData('portugal');
+// getCountryData('germany');
+// getCountryData('serbia');
+getCountryData('brazil');
