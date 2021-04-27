35317df5cd61ec845e0725e4c172a25968b520a5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-04-27 20:01:16 +0200

    The module pattern

--- 15 Modern-JS-Modules-Tooling/script.js
+++ 15 Modern-JS-Modules-Tooling/script.js
@@ -3,7 +3,7 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('importing module');
+// console.log('importing module');
 // console.log(shipingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -12,9 +12,39 @@ console.log('importing module');
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
 
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
 
-console.log(cart);
+// console.log(cart);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
